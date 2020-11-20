684571d85f4c2ec0eeb2259a351002b550ebdd03
Johan Lindkvist <johanlindkvist89@gmail.com>
2020-11-20 10:19:04 +0100

    Change cipher challenge description

--- src/challenges/CipherChallenge.jsx
+++ src/challenges/CipherChallenge.jsx
@@ -2,13 +2,23 @@ const CipherChallenge = () => {
   return (
     <>
       <p>
-        Ettersom julenissen må planlegge julen hjemmefra i år, så man han sende meldinger per post. Dette føler han ær svært riskabelt ettersom noen då kan få tag på hans planer og fortelle barna. For å løse dette har julenissen valgt att kryptere meldingerne sine med hjelp av Vigenère kryptering.
+        Covid-19 har ført til at julenissen må gjennomføre årets juleplanlegging fra hjemmekontor. På grunn av manglende digitalisering, må han benytte seg av tradisjonell brevpost for å kommunisere med sine kollegaer på verkstedet. Dette er veldig risikabelt, og han er redd for at utenforstående kan få tak i barnas personopplysninger - og kanskje enda verre - avsløre årets juleplaner overfor barna. 
       </p>
       <br />
       <p>
-        Du sitter nå på andre siden i julegavefabrikken og må dekryptere julenissens melding, hvilken lyder
-        <p><strong>Ji ggsp io cpbhnes ydrr khpegr iwiac</strong>.</p>
-        Alt du vet er at nyckelordet er <strong>NETCOMPANY</strong> og at julenissen liker å bruke det engelske alfabetet med versaler. Hva prøver julenissen å si?
+        For å løse dette problemet har Julenissen valgt å kryptere meldingene sine ved hjelp av Vigenére kryptering. Som nissens mest trofaste hjelper, har du fått ansvaret for å dekryptere meldingene og videreformidle de til de ansatte ved juleverkstedet. 
+      </p>
+      <br />
+      <p>Meldingen du nå skal dekryptere er som følger: </p>
+      <strong>Ji ggsp io cpbhnes ydrr khpegr iwiac</strong>
+      <br />
+      <br />
+      <p>
+        Det eneste du vet er at nøkkelordet er <strong>NETCOMPANY</strong> og at julenissen liker å bruke det engelske alfabetet med versaler.
+      </p>
+      <br />
+      <p>
+        Hva er det julenissen prøver å fortelle de ansatte ved juleverkstedet?
       </p>
     </>
   )
