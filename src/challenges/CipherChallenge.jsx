b6fe0ddf90ea6211ff05fe44728d9a3673db6e68
Johan Lindkvist <johanlindkvist89@gmail.com>
2020-11-20 09:48:04 +0100

    Add cipher challenge

--- src/challenges/CipherChallenge.jsx
+++ src/challenges/CipherChallenge.jsx
@@ -2,7 +2,13 @@ const CipherChallenge = () => {
   return (
     <>
       <p>
-        Idèn her er å ha en julesettning som krypteres, og så må studenterne dekryptere denne med hjelp av kode. Se for eksempel <a href='https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher' target='_blank' rel='noreferrer'>Vigenère cipher</a>
+        Ettersom julenissen må planlegge julen hjemmefra i år, så man han sende meldinger per post. Dette føler han ær svært riskabelt ettersom noen då kan få tag på hans planer og fortelle barna. For å løse dette har julenissen valgt att kryptere meldingerne sine med hjelp av Vigenère kryptering.
+      </p>
+      <br />
+      <p>
+        Du sitter nå på andre siden i julegavefabrikken og må dekryptere julenissens melding, hvilken lyder
+        <p><strong>Ji ggsp io cpbhnes ydrr khpegr iwiac</strong>.</p>
+        Alt du vet er at nyckelordet er <strong>NETCOMPANY</strong> og at julenissen liker å bruke det engelske alfabetet med versaler. Hva prøver julenissen å si?
       </p>
     </>
   )
